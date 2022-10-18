de3e11cc77a06623bc2e77048b502a56f3f7281a
minsang98 <blood89@nate.com>
2022-10-18 15:45:11 +0900

    add change

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,7 +1,8 @@
-import React, { memo } from "react";
+import React, { memo, useState, useRef } from "react";
 import styled from "styled-components";
-import { BsCheckSquareFill, BsCheckSquare } from "react-icons/bs";
+import { BsCheckSquareFill, BsCheckSquare, BsPencil } from "react-icons/bs";
 import { TiDeleteOutline } from "react-icons/ti";
+import axios from "axios";
 
 const TodoComponent = styled.div`
   width: 100%;
@@ -14,6 +15,16 @@ const TodoComponent = styled.div`
     align-items: center;
     font-weight: 700;
     border-bottom: 1px solid #eee;
+    input {
+      width: 100%;
+      padding: 0.5rem;
+      font-size: 1rem;
+      font-weight: 700;
+      margin: 1rem;
+      border: none;
+      /* background: ${({ screenMode }) => (screenMode ? "#202020" : "#fff")};
+      color: ${({ screenMode }) => (screenMode ? "#fff" : "#202020")}; */
+    }
     &.done {
       span {
         color: ${({ screenMode }) => (screenMode ? "#B2B2B2" : "#B2B2B2")};
@@ -33,12 +44,40 @@ const TodoComponent = styled.div`
 
 function Todo({ data, doneHandler, deleteHandler, screenMode }) {
   const { title, done, id } = data;
-
+  const [change, setChange] = useState(false);
+  const [newTitle, setNewTitle] = useState(title);
+  const inputRef = useRef();
+  const changeHandler = () => {
+    data.title = newTitle;
+    axios.patch(`http://localhost:3001/todos/${id}`, data);
+  };
   return (
     <TodoComponent screenMode={screenMode}>
       <div className={done ? "todo done" : "todo"}>
-        <span>{title}</span>
+        <span>
+          {change ? (
+            <input
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              ref={inputRef}
+            />
+          ) : (
+            title
+          )}
+        </span>
         <div className="action-icons">
+          <div
+            className="action-icon"
+            onClick={() => {
+              setChange(!change);
+              inputRef.current.focus();
+              if (change) {
+                changeHandler();
+              }
+            }}
+          >
+            <BsPencil />
+          </div>
           <div className="action-icon complete" onClick={() => doneHandler(id)}>
             {done ? <BsCheckSquareFill /> : <BsCheckSquare />}
           </div>
